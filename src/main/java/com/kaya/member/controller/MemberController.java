b161660e35455e350922c47123aaeefb50b415cb
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-06-28 01:42:07 +0900

    회원정보 상세조회
    Create detail.html

--- src/main/java/com/kaya/member/controller/MemberController.java
+++ src/main/java/com/kaya/member/controller/MemberController.java
@@ -5,10 +5,7 @@ import com.kaya.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -57,6 +54,13 @@ public class MemberController {
         // html로 가져갈 데이터가 있다면 model 사용
         model.addAttribute("memberList", memberDTOList);
         return "list";
+    }
+
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member", memberDTO);
+        return "detail";
     }
 
 }
