d1fe3937be3da95cc1bc532620a6a9aac30b67c7
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-06-27 23:44:39 +0900

    회원가입 완료
    dto -> entity

--- src/main/java/com/kaya/member/entity/MemberEntity.java
+++ src/main/java/com/kaya/member/entity/MemberEntity.java
@@ -1,5 +1,6 @@
 package com.kaya.member.entity;
 
+import com.kaya.member.dto.MemberDTO;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,4 +23,13 @@ public class MemberEntity {
 
     @Column
     private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
+
 }
--- src/main/java/com/kaya/member/service/MemberService.java
+++ src/main/java/com/kaya/member/service/MemberService.java
@@ -1,13 +1,21 @@
 package com.kaya.member.service;
 
 import com.kaya.member.dto.MemberDTO;
+import com.kaya.member.entity.MemberEntity;
+import com.kaya.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-//    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
     public void save(MemberDTO memberDTO) {
+        // 1. dto -> entity 변환
+        // 2. repository의 save메서드 호출
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+        memberRepository.save(memberEntity);
+        // repository의 save메서드 호출 (조건: entity 객체를 넘겨줘야 함)
+
     }
 }
