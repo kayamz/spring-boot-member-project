961e2079330925a37b76c652e5492e3185a20dc3
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-06-28 21:45:22 +0900

    ajax 이메일 중복체크
    구현 완료

--- src/main/java/com/kaya/member/controller/MemberController.java
+++ src/main/java/com/kaya/member/controller/MemberController.java
@@ -92,6 +92,12 @@ public class MemberController {
     @PostMapping("/member/email-check")
     public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail) {
         System.out.println("memberEmail = " + memberEmail);
-        return "체크완료";
+        String checkResult = memberService.emailCheck(memberEmail);
+        return checkResult;
+//        if (checkResult != null) {
+//            return "ok";
+//        } else {
+//            return "no";
+//        }
     }
 }
